92aa921926f1da1d9064d0f622a741b33d2fb2e4
rfiolka <roksana.fiolka@icloud.com>
2022-11-13 09:04:40 +0100

    fetch cat facts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import './App.css';
-import { useEffect, useState } from "react"
+import "./App.css";
+import { useEffect, useState } from "react";
+import Axios from "axios";
+import CatImg from './assets/cat-unsplash.jpg'
+
 
 function App() {
+  const [catFact, setCatFact] = useState("");
+
+  const fetchCatFact = () => {
+    Axios.get("https://catfact.ninja/fact").then((res) => {
+      setCatFact(res.data.fact)
+    });
+  }
+
+  useEffect(() => {
+    fetchCatFact();
+  }, []); // useEffecr runs once because of []
+
   return (
     <div className="App">
-  
+      <button onClick={fetchCatFact}> Generate Cat Fact</button>
+      <p> {catFact} </p>
+      <div>
+        <img src={CatImg} alt="cat" />
+      </div>
     </div>
   );
 }
